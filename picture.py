411061162d5dc78c4fd09966d59f9acaacb0dea8
minhqh <minh.hoquang.dev@gmail.com>
2025-08-01 15:30:27 +0700

    Thêm hàm hẹ hẹ

--- picture.py
+++ picture.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread("tsumiki.png")
 image = cv2.cvtColor(image , cv2.COLOR_RGB2BGR)
-image2 = cv2.cvtColor(image , cv2.COLOR_RGB2HLS)
 
 def image_show(image , title):
     plt.imshow(image)
     plt.title(title)
     plt.axis("off")
     plt.show()
+def change_image(image):
+    return cv2.cvtColor(image , cv2.COLOR_RGB2HLS)
+
+image2 = change_image(image)
 
 image_show(image2 , 'tsumiki2')
